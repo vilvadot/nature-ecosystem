3dd696fbdbb1603a162e1d166ea350c033ddeafd
vilvadot <hi@vilva.io>
2022-04-25 20:58:11 +0100

    Change image locations until I fix GH pages

--- organisms/Alga.js
+++ organisms/Alga.js
@@ -18,7 +18,7 @@ export class Alga extends Organism {
 
   _render() {
     return this.draw
-      .image('/organisms/seaweed.gif')
+      .image('/seaweed.gif')
       .size(this.size, this.size)
       .move(this.x, this.y - this.size)
   }
